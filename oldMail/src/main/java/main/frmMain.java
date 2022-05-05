f643b0d87ed6683858369bfcd5980ac55c75bb9b
Miguel Matul <miguemat4@gmail.com>
2022-05-04 18:30:31 -0600

    Problema base a resolver

--- oldMail/src/main/java/main/frmMain.java
+++ oldMail/src/main/java/main/frmMain.java
@@ -9,6 +9,29 @@ package main;
  * @author mfmatul
  */
 public class frmMain extends javax.swing.JFrame {
+    
+    private int contadorEmisores = 1;
+    private int contadorMensajeros = 1;
+    String[] mensajes = {"El Real Madrid es el mejor equipo del mundo", 
+        "DC es mejor que Marvel", 
+        "Sistemas Operativos fue el curso favorito de los estudiantes", 
+        "Ella no te ama :'c",
+        "La pizza con piña sí es buena",
+        "La paciencia es una virtud",
+        "En clases no hay dudas, pero en los exámenes sí ¬¬",
+        "Todos saben que Batman es mejor que Superman",
+        "Esperamos algún día volver a la normalidad",
+        "Puro manco estaba en el Team Iron Man",
+        "Todos sabemos que Capitán América tenía la razón en Civil War",
+        "Los tqm mis queridos estudiantes",
+        "¿Cuándo jugamos una partida de among us?",
+        "A los introvertidos no les afectó la pandemia",
+        "XV Simposio - Ingeniería que transforma al mundo",
+        "Todos saben que el frío es mejor que el calor",
+        "Todos saben que Cristiano Ronaldo es mejor que Messi",
+        "Hola, cómo están? Todo bien? Todo correcto?",
+        "Ya no sé que más escribir xd",
+        "Todos saben que la mayoría de estudiantes copia en la modalida virtual"};
 
     /**
      * Creates new form frmMain
@@ -18,6 +41,20 @@ public class frmMain extends javax.swing.JFrame {
         btnMenosEmisores.setEnabled(false);
         btnMenosMensajeros.setEnabled(false);
         txtBuzon.setEnabled(false);
+    }
+    
+    public class Emisor {
+        // Debe buscar un mensaje aleatorio (0-19)
+        // Si hay espacio en el buzón, debe colocar el mensaje en el text area
+        // Ojo que el text area debe indicar el orden de prioridad en base a las líneas
+        // El orden de ingreso es FIFO
+    }
+    
+    public class Mensajero {
+        // Debe buscar el mensaje con la prioridad más alta
+        // Libera del buzón el mensaje y lo debe mostrar en consola
+        // Ojo que el text area debe indicar que línea de mensaje ya fue entregado
+        // La línea que acaba de enviar debe mostrar un mensaje tipo: "Mensaje envíado"
     }
 
     /**
@@ -56,20 +93,40 @@ public class frmMain extends javax.swing.JFrame {
         jLabel3.setText("Emisores:");
 
         btnMenosEmisores.setText("-");
+        btnMenosEmisores.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMenosEmisoresActionPerformed(evt);
+            }
+        });
 
         lblEmisores.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         lblEmisores.setText("1");
 
         btnMasEmisores.setText("+");
+        btnMasEmisores.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMasEmisoresActionPerformed(evt);
+            }
+        });
 
         jLabel4.setText("Mensajeros:");
 
         btnMenosMensajeros.setText("-");
+        btnMenosMensajeros.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMenosMensajerosActionPerformed(evt);
+            }
+        });
 
         lblMensajeros.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         lblMensajeros.setText("1");
 
         btnMasMensajeros.setText("+");
+        btnMasMensajeros.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMasMensajerosActionPerformed(evt);
+            }
+        });
 
         jLabel5.setText("Central de correo:");
 
@@ -79,6 +136,11 @@ public class frmMain extends javax.swing.JFrame {
 
         btnIniciar.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         btnIniciar.setText("Iniciar programa");
+        btnIniciar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnIniciarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -162,6 +224,50 @@ public class frmMain extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnMasEmisoresActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMasEmisoresActionPerformed
+        contadorEmisores++;
+        lblEmisores.setText(String.valueOf(contadorEmisores));
+        if (contadorEmisores == 2)
+            btnMenosEmisores.setEnabled(true);
+        if (contadorEmisores == 5)
+            btnMasEmisores.setEnabled(false);
+    }//GEN-LAST:event_btnMasEmisoresActionPerformed
+
+    private void btnMenosEmisoresActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMenosEmisoresActionPerformed
+        contadorEmisores--;
+        lblEmisores.setText(String.valueOf(contadorEmisores));
+        if (contadorEmisores == 4)
+            btnMasEmisores.setEnabled(true);
+        if (contadorEmisores == 1)
+            btnMenosEmisores.setEnabled(false);
+    }//GEN-LAST:event_btnMenosEmisoresActionPerformed
+
+    private void btnMenosMensajerosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMenosMensajerosActionPerformed
+        contadorMensajeros--;
+        lblMensajeros.setText(String.valueOf(contadorMensajeros));
+        if (contadorMensajeros == 4)
+            btnMasMensajeros.setEnabled(true);
+        if (contadorMensajeros == 1)
+            btnMenosMensajeros.setEnabled(false);
+    }//GEN-LAST:event_btnMenosMensajerosActionPerformed
+
+    private void btnMasMensajerosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMasMensajerosActionPerformed
+        contadorMensajeros++;
+        lblMensajeros.setText(String.valueOf(contadorMensajeros));
+        if (contadorMensajeros == 2)
+            btnMenosMensajeros.setEnabled(true);
+        if (contadorMensajeros == 5)
+            btnMasMensajeros.setEnabled(false);
+    }//GEN-LAST:event_btnMasMensajerosActionPerformed
+
+    private void btnIniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIniciarActionPerformed
+        btnMenosEmisores.setEnabled(false);
+        btnMasEmisores.setEnabled(false);
+        btnMenosMensajeros.setEnabled(false);
+        btnMasMensajeros.setEnabled(false);
+        btnIniciar.setEnabled(false);
+    }//GEN-LAST:event_btnIniciarActionPerformed
 
     /**
      * @param args the command line arguments
